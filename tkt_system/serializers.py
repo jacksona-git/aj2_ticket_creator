42eab02b8ae3704b3702cb8621a8716e24057890
jacksona-git <61547865+jacksona-git@users.noreply.github.com>
2020-02-28 08:29:10 -0500

    Update serializers.py

--- tkt_system/serializers.py
+++ tkt_system/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Ticket
 from django.contrib.auth.models import User
 
-
+#Class to convert ticket modal to JSON
 class TicketSerializer(serializers.ModelSerializer):  # create class to serializer model
     creator = serializers.ReadOnlyField(source='creator.username')
 
@@ -10,7 +10,7 @@ class TicketSerializer(serializers.ModelSerializer):  # create class to serializ
         model = Ticket
         fields = ('id', 'problem_desc', 'asset_name', 'asset_id', 'creator')
 
-
+#class to serialize user details
 class UserSerializer(serializers.ModelSerializer):  # create class to serializer usermodel
     tickets = serializers.PrimaryKeyRelatedField(many=True, queryset=Ticket.objects.all())
 
