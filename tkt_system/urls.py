b537ccd7c13d17a75b7b18cc48deab9ea68bc1a2
jacksona-git <61547865+jacksona-git@users.noreply.github.com>
2020-02-28 09:35:59 -0500

    Update urls.py
    update comments

--- tkt_system/urls.py
+++ tkt_system/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
 from . import views
 
-
+#map the url endpoint to the correct view
 urlpatterns = [
     re_path(r'^api/v1/tickets/(?P<pk>[0-9]+)$', # Url to get update or delete a ticket
         views.get_delete_update_ticket.as_view(),
