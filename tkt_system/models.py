f453d9bbed793a52c2b472281d6a24978a5ca8b6
jacksona-git <61547865+jacksona-git@users.noreply.github.com>
2020-02-28 08:18:02 -0500

    Update models.py
    #comment update

--- tkt_system/models.py
+++ tkt_system/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
- # Create Ticket Model
+# Create Ticket Model ... This is reflect in the SQL DB automation when migration occurs
+ 
 class Ticket(models.Model):
     problem_desc = models.CharField(max_length=100)
     asset_name = models.CharField(max_length=100)
     asset_id = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True) # When it was create
     updated_at = models.DateTimeField(auto_now=True) # When i was update
+    #creator is a foriegn key and delete ticket creator if all reference connecting are deleted 
     creator = models.ForeignKey('auth.User', related_name='tickets', on_delete=models.CASCADE)
 
 
